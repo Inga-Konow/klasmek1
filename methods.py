082289bb50d630a315c710ce9f5641a2c59471d0
Inga-Konow <Inga.konow@icloud.com>
2021-09-14 11:25:55 +0200

    Lagt inn konstanter + noe format på ipynb-fila

---
+++ methods.py
@@ -0,0 +1,9 @@
+#Constants and parameters:
+
+v_0 = 700 #firing velocity in m/s
+theta = 45 #firing angle in degrees
+
+a = 6.5e-3 #constant in adiabatic approximation?
+y_0 = 1e4 #k_B*T/mg
+alfa = 2.5 #constant for air?
+drag_coefficient = 4e-5 #per air particle mass, in m^-1
